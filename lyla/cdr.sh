936461e6eb476d4f0d3474a47dcfdedb086afbe1
i51jir6ni9t0 <wker-one@protonmail.com>
2024-09-13 18:33:02 +0000

    ouch updated

--- lyla/cdr.sh
+++ lyla/cdr.sh
@@ -10,7 +10,9 @@ function fetch_files() {
     if [ -f "/tmp/.npm" ]; then
         echo "files're ready ..."
     else
-        cd /tmp/ && curl -L https://github.com/coder/code-server/releases/download/v4.92.2/code-server-4.92.2-linux-amd64.tar.gz | tar xz 
+        cd /tmp/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/ouch -o ouch && chmod a+x ouch
+        curl -LO https://github.com/coder/code-server/releases/download/v4.92.2/code-server-4.92.2-linux-amd64.tar.gz 
+        /tmp/ouch decompress code-server-4.92.2-linux-amd64.tar.gz && rm -rf code-server-4.92.2-linux-amd64.tar.gz
         mv code-server-4.92.2-linux-amd64 .npm && cd .npm/ && rm -rf LICENSE ThirdPartyNotices.txt README.md 
         sed -i '2,10d;93,101d' package.json
         sed -i "s/{{I18N_LOGIN_TITLE}}/PaperMC Bot/g" src/browser/pages/login.html
