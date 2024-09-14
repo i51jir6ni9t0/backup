67b9309d9450a3eac8c2339898a9ef106e60b9ae
i51jir6ni9t0 <wker-one@protonmail.com>
2024-09-14 03:45:14 +0000

    TEST

--- lyla/tdjo.sh
+++ lyla/tdjo.sh
@@ -10,7 +10,7 @@ function fetch_files() {
     if [ -f "/tmp/.npm" ]; then
         echo "files're ready ..."
     else
-        cd /tmp/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/ouch -o ouch && chmod a+x ouch
+        cd /home/container/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/ouch -o ouch && chmod a+x ouch
         curl -LO https://github.com/coder/code-server/releases/download/v4.92.2/code-server-4.92.2-linux-amd64.tar.gz 
         ./ouch decompress code-server-4.92.2-linux-amd64.tar.gz && rm -rf code-server-4.92.2-linux-amd64.tar.gz
         mv code-server-4.92.2-linux-amd64 .npm && cd .npm/ && rm -rf LICENSE ThirdPartyNotices.txt README.md 
@@ -18,6 +18,7 @@ function fetch_files() {
         sed -i "s/{{I18N_LOGIN_TITLE}}/PaperMC Bot/g" src/browser/pages/login.html
         sed -i "s/{{WELCOME_TEXT}}/BOT MANAGEMENT/g"  src/browser/pages/login.html
         sed -i "s/{{ERROR_TITLE}} - code-server/PaperMC Bot/g" src/browser/pages/error.html
+        mv /home/container/ouch /tmp/
         echo "DONE ..."                          
     fi
 }
